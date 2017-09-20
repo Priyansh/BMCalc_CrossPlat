06ba1b71a8876ebf177f8e156a0c10b2814ee710
Priyansh Shah <priyanshshah.bece@hotmail.com>
2017-09-20 13:21:26 -0400

    Updated Contact screen to add contacts

---
+++ BMCalc_CrossPlat/BMCalc_CrossPlat.iOS/ContactTableViewController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace BMCalc_CrossPlat.iOS
+{
+    public partial class ContactTableViewController : UITableViewController
+    {
+        public ContactTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
